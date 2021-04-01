e71d74327e4611945c220dc3dede41483a731302
Mounishag2712 <81089070+Mounishag2712@users.noreply.github.com>
2021-04-01 08:45:03 +0530

    Add files via upload

---
+++ AutoIdentification212/Auto-Identification/src/main/java/com/autoidentification/AutoIdentificationApplication.java
@@ -0,0 +1,13 @@
+package com.autoidentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoIdentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoIdentificationApplication.class, args);
+	}
+
+}
